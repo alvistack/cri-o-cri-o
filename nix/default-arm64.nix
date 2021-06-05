e91c8f384f0ff89433434678e073dcd6764c62dc
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2021-06-05 23:26:45 +0800

    nix build f nix/

--- nix/default-arm64.nix
+++ nix/default-arm64.nix
@@ -25,6 +25,21 @@ let
               -i "$dev"/include/glib-2.0/gobject/gobjectnotifyqueue.c
           '';
         });
+        pcsclite = (static pkg.pcsclite).overrideAttrs (x: {
+          configureFlags = [
+            "--enable-confdir=/etc"
+            "--enable-usbdropdir=/var/lib/pcsc/drivers"
+            "--disable-libsystemd"
+          ];
+          buildInputs = [ pkgs.python3 pkgs.udev pkgs.dbus pkgs.systemd ];
+        });
+        systemd = (static pkg.systemd).overrideAttrs (x: {
+          outputs = [ "out" "dev" ];
+          mesonFlags = x.mesonFlags ++ [
+            "-Dglib=false"
+            "-Dstatic-libsystemd=true"
+          ];
+        });
       };
     };
   });
@@ -40,38 +55,20 @@ let
     enableStatic = true;
   });
 
-  self = with pkgs; buildGoModule {
+  self = with pkgs; buildGoModule rec {
     name = "cri-o";
     src = ./..;
     vendorSha256 = null;
     doCheck = false;
     enableParallelBuilding = true;
     outputs = [ "out" ];
-    nativeBuildInputs = with buildPackages; [
-      bash
-      gitMinimal
-      go-md2man
-      installShellFiles
-      makeWrapper
-      pkg-config
-      which
-    ];
-    buildInputs = [
-      glibc
-      glibc.static
-      gpgme
-      libassuan
-      libgpgerror
-      libseccomp
-      libapparmor
-      libselinux
-    ];
+    nativeBuildInputs = [ bash gitMinimal go-md2man pkg-config which ];
+    buildInputs = [ glibc glibc.static glib gpgme libassuan libgpgerror libseccomp libapparmor libselinux ];
     prePatch = ''
       export CFLAGS='-static -pthread'
       export LDFLAGS='-s -w -static-libgcc -static'
       export EXTRA_LDFLAGS='-s -w -linkmode external -extldflags "-static -lm"'
       export BUILDTAGS='static netgo osusergo exclude_graphdriver_btrfs exclude_graphdriver_devicemapper seccomp apparmor selinux'
-      export CGO_ENABLED=1
     '';
     buildPhase = ''
       patchShebangs .
